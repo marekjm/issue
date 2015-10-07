56bf4d79f2927b5119cb93c9ae9d1650d8e7aaa4
Marek Marecki <marekjm@ozro.pw>
2015-10-07 09:15:57 +0200

    Initial commit

---
+++ issue.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import hashlib
+import json
+import os
+import random
+import sys
+
+import clap
+
+
+__version__ = '0.0.0'
+
+
+filename_ui = os.path.expanduser('~/.local/share/issue/ui.json')
+
+model = {}
+with open(filename_ui, 'r') as ifstream: model = json.loads(ifstream.read())
+
+args = list(clap.formatter.Formatter(sys.argv[1:]).format())
+
+command = clap.builder.Builder(model).insertHelpCommand().build().get()
+parser = clap.parser.Parser(command).feed(args)
+checker = clap.checker.RedChecker(parser)
+
+
+try:
+    err = None
+    checker.check()
+    fail = False
+except clap.errors.MissingArgumentError as e:
+    print('missing argument for option: {0}'.format(e))
+    fail = True
+except clap.errors.UnrecognizedOptionError as e:
+    print('unrecognized option found: {0}'.format(e))
+    fail = True
+except clap.errors.ConflictingOptionsError as e:
+    print('conflicting options found: {0}'.format(e))
+    fail = True
+except clap.errors.RequiredOptionNotFoundError as e:
+    fail = True
+    print('required option not found: {0}'.format(e))
+except clap.errors.InvalidOperandRangeError as e:
+    print('invalid number of operands: {0}'.format(e))
+    fail = True
+except clap.errors.UIDesignError as e:
+    print('UI has design error: {0}'.format(e))
+    fail = True
+except Exception as e:
+    print('fatal: unhandled exception: {0}: {1}'.format(str(type(e))[8:-2], e))
+    fail, err = True, e
+finally:
+    if fail: exit(1)
+    ui = parser.parse().ui().finalise()
+
+
+if '--version' in ui:
+    print('issue version {0}'.format(__version__))
+    exit(0)
+if clap.helper.HelpRunner(ui=ui, program=sys.argv[0]).adjust(options=['-h', '--help']).run().displayed(): exit(0)
+
+
+
+# ensure the repository exists
+REPOSITORY_PATH = os.path.expanduser('~/.issue')
+OBJECTS_PATH = os.path.join(REPOSITORY_PATH, 'objects')
+ISSUES_PATH = os.path.join(OBJECTS_PATH, 'issues')
+LABELS_PATH = os.path.join(OBJECTS_PATH, 'labels')
+MILESTONES_PATH = os.path.join(OBJECTS_PATH, 'milestones')
+
+for pth in (REPOSITORY_PATH, OBJECTS_PATH, ISSUES_PATH, LABELS_PATH, MILESTONES_PATH):
+    if not os.path.isdir(pth):
+        os.mkdir(pth)
+
+
+
+ui = ui.down() # go down a mode
+operands = ui.operands()
+
+if str(ui) == 'open':
+    message = ''
+    if len(operands) < 1:
+        message = input('issue description: ')
+    else:
+        message = operands[0]
+    labels = [l[0] for l in ui.get('--label')]
+    milestones = [m[0] for m in ui.get('--milestone')]
+    print(message)
+    print(labels)
+    print(milestones)
+
+    issue_sha1 = '{0}{1}{2}{3}'.format(message, labels, milestones, random.random())
+    issue_sha1 = hashlib.sha1(issue_sha1.encode('utf-8')).hexdigest()
+    print(issue_sha1)
+elif str(ui) == 'close':
+    print()
