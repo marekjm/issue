41d413a21bc9336e54b8efab6e9c94efac1353d3
Marek Marecki <marekjm@ozro.pw>
2015-11-20 13:08:19 +0100

    Placeholder code for "show log" command

--- issue.py
+++ issue.py
@@ -1192,7 +1192,8 @@ def commandParam(ui):
     indexIssue(issue_sha1, issue_diff_sha1)
 
 def commandShow(ui):
-    issue_sha1 = (getLastIssue() if '--last' in ui else operands[0])
+    ui = ui.down()
+    issue_sha1 = (getLastIssue() if '--last' in ui else ui.operands()[0])
     try:
         issue_sha1 = expandIssueUID(issue_sha1)
     except issue.exceptions.IssueUIDAmbiguous:
@@ -1209,41 +1210,44 @@ def commandShow(ui):
         print('fatal: {0} does not identify a valid object'.format(repr(issue_sha1)))
         exit(1)
 
-    issue_message_lines = issue_data['message'].splitlines()
+    if str(ui) == 'show':
+        issue_message_lines = issue_data['message'].splitlines()
 
-    issue_open_author_name = (issue_data['open.author.name'] if 'open.author.name' in issue_data else 'Unknown Author')
-    issue_open_author_email = (issue_data['open.author.email'] if 'open.author.email' in issue_data else 'Unknown email')
-    issue_open_timestamp = (datetime.datetime.fromtimestamp(issue_data['open.timestamp']) if 'open.timestamp' in issue_data else 'unknown date')
-    print('{0}: {1}'.format(issue_sha1, issue_message_lines[0]))
-    print('    opened by:  {0} ({1}), on {2}'.format(issue_open_author_name, issue_open_author_email, issue_open_timestamp))
-    if issue_data['status'] == 'closed':
-        issue_close_author_name = (issue_data['close.author.name'] if 'close.author.name' in issue_data else 'Unknown Author')
-        issue_close_author_email = (issue_data['close.author.email'] if 'close.author.email' in issue_data else 'Unknown email')
-        issue_close_timestamp = (datetime.datetime.fromtimestamp(issue_data['close.timestamp']) if 'close.timestamp' in issue_data else 'unknown date')
-        print('    closed by:  {0} ({1}), on {2}'.format(issue_close_author_name, issue_close_author_email, issue_close_timestamp))
-    print('    milestones: {0}'.format(', '.join(issue_data['milestones'])))
-    print('    tags:       {0}'.format(', '.join(issue_data['tags'])))
-    print('    project:    {0} ({1})'.format(issue_data.get('project.name', 'Unknown'), issue_data.get('project.tag', '')))
-    if 'parameters' in issue_data and issue_data['parameters']:
-        print('\n---- PARAMETERS')
-        for key in sorted(issue_data['parameters'].keys()):
-            print('    {0} = {1}'.format(key, issue_data['parameters'][key]))
+        issue_open_author_name = (issue_data['open.author.name'] if 'open.author.name' in issue_data else 'Unknown Author')
+        issue_open_author_email = (issue_data['open.author.email'] if 'open.author.email' in issue_data else 'Unknown email')
+        issue_open_timestamp = (datetime.datetime.fromtimestamp(issue_data['open.timestamp']) if 'open.timestamp' in issue_data else 'unknown date')
+        print('{0}: {1}'.format(issue_sha1, issue_message_lines[0]))
+        print('    opened by:  {0} ({1}), on {2}'.format(issue_open_author_name, issue_open_author_email, issue_open_timestamp))
+        if issue_data['status'] == 'closed':
+            issue_close_author_name = (issue_data['close.author.name'] if 'close.author.name' in issue_data else 'Unknown Author')
+            issue_close_author_email = (issue_data['close.author.email'] if 'close.author.email' in issue_data else 'Unknown email')
+            issue_close_timestamp = (datetime.datetime.fromtimestamp(issue_data['close.timestamp']) if 'close.timestamp' in issue_data else 'unknown date')
+            print('    closed by:  {0} ({1}), on {2}'.format(issue_close_author_name, issue_close_author_email, issue_close_timestamp))
+        print('    milestones: {0}'.format(', '.join(issue_data['milestones'])))
+        print('    tags:       {0}'.format(', '.join(issue_data['tags'])))
+        print('    project:    {0} ({1})'.format(issue_data.get('project.name', 'Unknown'), issue_data.get('project.tag', '')))
+        if 'parameters' in issue_data and issue_data['parameters']:
+            print('\n---- PARAMETERS')
+            for key in sorted(issue_data['parameters'].keys()):
+                print('    {0} = {1}'.format(key, issue_data['parameters'][key]))
 
-    print('\n---- MESSAGE')
-    print('\n  {0}\n'.format('\n  '.join(issue_message_lines)))
+        print('\n---- MESSAGE')
+        print('\n  {0}\n'.format('\n  '.join(issue_message_lines)))
 
-    if 'closing_git_commit' in issue_data:
-        print('\n---- CLOSING GIT COMMIT: {0}\n'.format(issue_data['closing_git_commit']))
+        if 'closing_git_commit' in issue_data:
+            print('\n---- CLOSING GIT COMMIT: {0}\n'.format(issue_data['closing_git_commit']))
 
-    issue_comment_thread = dict((issue_data['comments'][key]['timestamp'], key) for key in issue_data['comments'])
-    if issue_comment_thread:
-        print('\n---- COMMENT THREAD:\n')
-        for i, timestamp in enumerate(sorted(issue_comment_thread.keys())):
-            issue_comment = issue_data['comments'][issue_comment_thread[timestamp]]
-            print('>>>> {0}. {1} ({2}) at {3}\n'.format(i, issue_comment['author.name'], issue_comment['author.email'], datetime.datetime.fromtimestamp(issue_comment['timestamp'])))
-            print(issue_comment['message'])
-            print()
-    markLastIssue(issue_sha1)
+        issue_comment_thread = dict((issue_data['comments'][key]['timestamp'], key) for key in issue_data['comments'])
+        if issue_comment_thread:
+            print('\n---- COMMENT THREAD:\n')
+            for i, timestamp in enumerate(sorted(issue_comment_thread.keys())):
+                issue_comment = issue_data['comments'][issue_comment_thread[timestamp]]
+                print('>>>> {0}. {1} ({2}) at {3}\n'.format(i, issue_comment['author.name'], issue_comment['author.email'], datetime.datetime.fromtimestamp(issue_comment['timestamp'])))
+                print(issue_comment['message'])
+                print()
+        markLastIssue(issue_sha1)
+    elif str(ui) == 'log':
+        print('showing log of issue: {0}'.format(issue_sha1))
 
 def commandConfig(ui):
     ui = ui.down()
