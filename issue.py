b101e95beeac39f84c8fe07595b16612a85064f1
Marek Marecki <marekjm@ozro.pw>
2018-07-03 10:19:43 +0200

    Add basic events log

--- issue.py
+++ issue.py
@@ -115,6 +115,7 @@ COLOR_WARNING = 'red'
 COLOR_NOTE = 'blue'
 COLOR_HASH = 'yellow'
 COLOR_BRANCH_NAME = 'white'
+COLOR_DATETIME = 'cyan'
 
 
 # issue-related utility functions
@@ -891,6 +892,190 @@ def repositoryInit(force=False, up=False):
 
 
 ######################################################################
+# LOG UTILITY FUNCTIONS
+#
+def get_events_log_path():
+    return os.path.expanduser(os.path.join('~', '.local', 'log', 'issue'))
+
+def read_events_log():
+    pth = get_events_log_path()
+    if not os.path.isdir(pth):
+        return []
+    events_log = []
+    events_log_path = os.path.join(pth, 'events_log.json')
+    if os.path.isfile(events_log_path):
+        with open(events_log_path) as ifstream:
+            events_log = json.loads(ifstream.read())
+    return events_log
+
+def write_events_log(events_log):
+    pth = get_events_log_path()
+    if not os.path.isdir(pth):
+        os.makedirs(pth)
+    with open(os.path.join(pth, 'events_log.json'), 'w') as ofstream:
+        events_log_size = getConfig().get('events_log_size', EVENTS_LOG_SIZE_DEFAULT)
+        ofstream.write(json.dumps(events_log[-events_log_size:]))
+
+EVENTS_LOG_SIZE_DEFAULT = 80
+
+EVENT_TYPE_SHOW = 'show'
+EVENT_TYPE_SLUG = 'slug'
+EVENT_TYPE_COMMENT = 'comment'
+
+EVENTS_LOG_EVENT_WEIGHTS = {
+    EVENT_TYPE_SHOW: 10,
+    EVENT_TYPE_SLUG: 0,
+    EVENT_TYPE_COMMENT: 7,
+}
+
+def append_events_log_event(issue_uid, content):
+    pth = get_events_log_path()
+    events_log = read_events_log()
+    content['issue_uid'] = issue_uid
+    content['timestamp'] = timestamp()
+    if events_log and (events_log[-1].get('event') == content.get('event') and events_log[-1].get('issue_uid') == 'issue_uid'):
+        return
+    events_log.append(content)
+    write_events_log(events_log)
+
+def add_events_log_event_show(issue_uid):
+    append_events_log_event(issue_uid = issue_uid, content = {
+        'event': EVENT_TYPE_SHOW,
+    })
+
+def add_events_log_event_slug(issue_uid, issue_slug):
+    append_events_log_event(issue_uid = issue_uid, content = {
+        'event': EVENT_TYPE_SLUG,
+        'parameters': {
+            'slug': issue_slug,
+        },
+    })
+
+def display_events_log(events_log, head=None, tail=None):
+    if head is not None:
+        events_log = events_log[:head]
+    if tail is not None:
+        events_log = events_log[tail:]
+    try:
+        for event in events_log:
+            event_name = event['event']
+            event_description = event.get('parameters', {})
+            if event_name == 'show':
+                event_description = getIssue(event['issue_uid']).get('message').splitlines()[0]
+            elif event_name == 'slug':
+                event_description = 'sluggified to {}'.format(colorise_repr(
+                    COLOR_BRANCH_NAME,
+                    event_description['slug'],
+                ))
+            elif event_name == 'comment':
+                comment_lines = event['parameters']['comment'].splitlines()
+                event_description = '{}'.format(comment_lines[0].strip())
+                if len(comment_lines) > 1:
+                    event_description += ' (...)'
+            else:
+                # if no special description formatting is provided, just display name of the event
+                event_description = ''
+            if event_description:
+                event_description = '{}'.format(event_description)
+            event_datetime = datetime.datetime.utcfromtimestamp(event.get('timestamp')).strftime('%Y-%m-%d %H:%M:%S')
+            print('{issue_key} [{event_datetime}] {event_name}{event_description}'.format(
+                issue_key = colorise(COLOR_HASH, event['issue_uid']),
+                event_datetime = colorise(COLOR_DATETIME, event_datetime),
+                event_name = event_name,
+                event_description = (': ' * bool(event_description)) + event_description,
+            ))
+    except BrokenPipeError as e:
+        # just silence this
+        # this exception is thrown when events_log is piped to head
+        pass
+
+def _bug_event_without_assigned_weight(event):
+    print('{}: {}: event {} does not have a weight assigned'.format(colorise(COLOR_WARNING, 'warning'), colorise(COLOR_ERROR, 'bug'), colorise_repr(COLOR_LABEL, event['event'])))
+
+def squash_events_log_aggressive_1(events_log):
+    """Aggressive-squash-1 assumes that basic squashing has
+    already been performed.
+    """
+    if len(events_log) < 2:
+        return events_log
+    squashed_events_log = [events_log[0]]
+    for event in events_log[1:]:
+        if event['issue_uid'] == squashed_events_log[-1]['issue_uid']:
+            last_event_action = EVENTS_LOG_EVENT_WEIGHTS.get(squashed_events_log[-1]['event'])
+            this_event_action = EVENTS_LOG_EVENT_WEIGHTS.get(event['event'])
+
+            if last_event_action is None:
+                _bug_event_without_assigned_weight(squashed_events_log[-1])
+            if this_event_action is None:
+                _bug_event_without_assigned_weight(event)
+            if last_event_action is None or this_event_action is None:
+                # zero out the comparison when an event does not have a weight assigned
+                last_event_action, this_event_action = 0, 0
+
+            if last_event_action > this_event_action:
+                squashed_events_log.pop()
+            elif last_event_action < this_event_action:
+                continue
+            else:
+                pass
+        squashed_events_log.append(event)
+    return squashed_events_log
+
+def rfind_if(seq, pred):
+    index = len(seq)-1
+    while index > -1:
+        if pred(seq[index]):
+            break
+        index -= 1
+    return index
+
+def squash_events_log_aggressive_2(events_log):
+    """Aggressive-squash-2 assumes that basic squashing, and
+    aggressive-squashing-1 have already been performed.
+    """
+    if len(events_log) < 2:
+        return events_log
+    squashed_events_log = [events_log[0]]
+    for event in events_log[1:]:
+        this_event_action = EVENTS_LOG_EVENT_WEIGHTS.get(event['event'])
+        index_of_last_event_for_the_same_issue = rfind_if(squashed_events_log, lambda e: e['issue_uid'] ==
+                event['issue_uid'])
+        if index_of_last_event_for_the_same_issue > -1:
+            last_event_action = EVENTS_LOG_EVENT_WEIGHTS.get(squashed_events_log[index_of_last_event_for_the_same_issue]['event'])
+
+            if last_event_action is None:
+                _bug_event_without_assigned_weight(squashed_events_log[-1])
+            if this_event_action is None:
+                _bug_event_without_assigned_weight(event)
+            if last_event_action is None or this_event_action is None:
+                # zero out the comparison when an event does not have a weight assigned
+                last_event_action, this_event_action = 0, 0
+
+            if last_event_action > this_event_action:
+                squashed_events_log.pop()
+            elif last_event_action < this_event_action:
+                continue
+            else:
+                pass
+        squashed_events_log.append(event)
+    return squashed_events_log
+
+def squash_events_log(events_log, aggressive=0):
+    if len(events_log) < 2:
+        return events_log
+    squashed_events_log = [events_log[0]]
+    for event in events_log[1:]:
+        if event['issue_uid'] == squashed_events_log[-1]['issue_uid'] and event['event'] == squashed_events_log[-1]['event']:
+            continue
+        squashed_events_log.append(event)
+    if aggressive > 0:
+        squashed_events_log = squash_events_log_aggressive_1(squashed_events_log)
+    if aggressive > 1:
+        squashed_events_log = squash_events_log_aggressive_2(squashed_events_log)
+    return squashed_events_log
+
+
+######################################################################
 # BACKEND FUNCTIONS
 #
 def get_release_path(release_name):
@@ -1602,6 +1787,7 @@ def commandSlug(ui):
             exit(r)
     if ('--git-branch' not in ui) and ('--git-checkout' not in ui):
         print(issue_slug)
+    add_events_log_event_slug(issue_sha1, issue_slug)
     markLastIssue(issue_sha1)
 
 def commandComment(ui):
@@ -1797,6 +1983,8 @@ def commandShow(ui):
         exit(1)
 
     if str(ui) == 'show':
+        add_events_log_event_show(issue_sha1)
+
         issue_message_lines = issue_data['message'].splitlines()
 
         issue_open_author_name = (issue_data['open.author.name'] if 'open.author.name' in issue_data else 'Unknown Author')
@@ -1894,6 +2082,8 @@ def commandShow(ui):
                 print()
         markLastIssue(issue_sha1)
     elif str(ui) == 'log':
+        add_events_log_event_show(issue_sha1)
+
         issue_sha1_heading = issue_sha1
         if colored: issue_sha1_heading = (colored.fg('yellow') + issue_sha1_heading + colored.attr('reset'))
         print('showing log of issue: {0}'.format(issue_sha1_heading))
@@ -2328,7 +2518,6 @@ def commandStatistics(ui):
             print('    total: {}'.format(total_lifetime_of_all))
             print('    avg:   {}'.format(avg_lifetime_of_all))
 
-
 def commandReleaseOpen(ui):
     ui = ui.down()
     release_name = ui.operands()[0]
@@ -2421,6 +2610,31 @@ def commandRelease(ui):
     elif str(ui) == 'notes':
         commandReleaseNotes(ui)
 
+def commandLog(ui):
+    ui = ui.down()
+    events_log = read_events_log()
+    events_log.reverse()
+    if str(ui) == 'squash':
+        initial_size = len(events_log)
+        if initial_size < 2:
+            print('{}: events_log too short to shorten'.format(colorise(COLOR_WARNING, 'warning')))
+            return
+        squashed_events_log = squash_events_log(events_log, aggressive = ui.get('--aggressive'))
+        final_size = len(squashed_events_log)
+        if final_size < initial_size:
+            print('{}: shortened events_log from {} to {} entries'.format(colorise(COLOR_NOTE, 'note'), initial_size, final_size))
+        write_events_log(squashed_events_log)
+        if '--verbose' in ui:
+            display_events_log(squashed_events_log)
+    else:
+        head = None
+        tail = None
+        if '--head' in ui:
+            head = ui.get('-H')
+        if '--tail' in ui:
+            tail = ui.get('-T')
+        display_events_log(events_log, head=head, tail=tail)
+
 
 def dispatch(ui, *commands, overrides = {}, default_command=''):
     """Semi-automatic command dispatcher.
@@ -2467,4 +2681,5 @@ dispatch(ui,        # first: pass the UI object to dispatch
     commandChain,
     commandStatistics,
     commandRelease,
+    commandLog,
 )
